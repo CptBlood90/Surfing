b5f553843f4145cf36f41e464d0964b8cd710be9
CptBlood90 <113608790+CptBlood90@users.noreply.github.com>
2022-12-12 19:27:01 +0300

    Добавил последние секции и карту.

---
+++ map.js
@@ -0,0 +1,30 @@
+let myMap;
+const init = () => {
+  myMap = new ymaps.Map("map", {
+    center: [55.75399400, 37.62209300],
+    zoom: 11,
+    controls: [],
+  });
+
+  let coords = [
+      [55.75312576, 37.59531092],
+      
+    ],
+    myCollection = new ymaps.GeoObjectCollection({}, {
+      draggable: false,
+      iconLayout: 'default#image',
+      iconImageHref: '../../img/icons/marker.svg',
+      iconImageSize: [46, 57],
+      iconImageOffset: [-35, -52]
+    });
+
+  for (let i = 0; i < coords.length; i++) {
+    myCollection.add(new ymaps.Placemark(coords[i]));
+  }
+
+  myMap.geoObjects.add(myCollection);
+
+  myMap.behaviors.disable('scrollZoom');
+};
+
+ymaps.ready(init);
